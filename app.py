366a04a5c89f14d5fabe2d5e46fc99e90e816c35
can aydin <canaydindev@gmail.com>
2024-08-06 23:51:55 +0300

    lasttt1122

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Bu satır CORS izinlerini ekler
 
-# Modeli yükleyin
+
 model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")

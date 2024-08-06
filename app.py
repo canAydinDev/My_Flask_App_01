86ed3592eb2e794bbb577671aa51d2d2e6ebe5db
can aydin <canaydindev@gmail.com>
2024-08-07 00:10:48 +0300

    garanti

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 import numpy as np
-from flask import Flask, request, jsonify
+
+from flask import Flask, request, render_template
+
 import pickle
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)  # Bu satır tüm origin'lerden gelen istekleri kabul eder
+CORS(app)
 
-# Modeli yükleyin
 model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to the Prediction Model API</h1>"
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    try:
-        int_features = [int(x) for x in request.form.values()]
-        final_features = np.array([int_features])
-        prediction = model.predict(final_features)
-        output = round(prediction[0], 2)
-        return jsonify({"prediction": output})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+    int_features = [int(x) for x in request.form.values()]
+    final_features = np.array([int_features])
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    
+    return {"prediction": output}
 
 if __name__ == "__main__":
     app.run(debug=True)
